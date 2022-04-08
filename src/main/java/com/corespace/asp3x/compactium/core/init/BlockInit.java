2ff9df81021e5e4fd8670b7ddfd3a7bdc4ccfb1c
AsP3X <zombymedia@gmail.com>
2022-04-08 16:42:12 +0200

    TASK: refactored the registration process of the block and block item TASK: added a block item auto register

--- src/main/java/com/corespace/asp3x/compactium/core/init/BlockInit.java
+++ src/main/java/com/corespace/asp3x/compactium/core/init/BlockInit.java
@@ -1,7 +1,10 @@
 package com.corespace.asp3x.compactium.core.init;
 
 import com.corespace.asp3x.compactium.Compactium;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
@@ -9,14 +12,35 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class BlockInit {
-    private BlockInit() {}
+    // Register for Blocks
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
+            Compactium.MOD_ID);
 
-    // create a item register variable
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Compactium.MOD_ID);
+    // Register for Block items
+    public static final DeferredRegister<Item> ITEMS = ItemInit.ITEMS;
 
-    // register a new block
-    public static final RegistryObject<Block> COMPACTIUM_BLOCK = BLOCKS.register("compactium_block",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.TERRACOTTA_BLUE)
-                    .strength(2.0f, 15f).requiresCorrectToolForDrops().friction(0.5f)));
+    /* Register Blocks here */
+
+    public static final RegistryObject<Block> COMPACTIUM_BLOCK = register("compactium_block", () ->
+            new Block(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_GREEN)
+                    .requiresCorrectToolForDrops()
+                    .strength(3.0f)
+                    .sound(SoundType.METAL)),
+            object -> () -> new BlockItem(object.get(), new Item.Properties().tab(Compactium.COMPACTIUM_TAB)));
+
+    /* Register Logic for the Block and Block Item */
+    private static <T extends Block> RegistryObject<T> registerBlock(final String name, final Supplier<? extends T> block) {
+        return BLOCKS.register(name, block);
+    }
+
+    private static <T extends Block> RegistryObject<T> register(final String name, final Supplier<? extends T> block,
+                                                                Function<RegistryObject<T>, Supplier<? extends Item>> item) {
+        RegistryObject<T> obj = registerBlock(name, block);
+        ITEMS.register(name, item.apply(obj));
+        return obj;
+    }
 }
